6dc20ef322b1afe7b22c702f2c459415a319edeb
DRRobertson9673 <drrobertson9673@gmail.com>
2023-01-22 15:58:38 +0000

    added comments to js, css and html

--- assets/script.js
+++ assets/script.js
@@ -1,9 +1,12 @@
+// variables for the script
 var displayDate = $('#currentDay')
 var hourNow = moment().format("H")
 var rightNow = moment().format("dddd, MMM Qo");
 
+// displays the day and date
 displayDate.text(rightNow)
 
+// sets the colours for past, present and future to each time block and pulls any content from local storage
 $(".time-block").each(function () {
     var blockHour = (this).id;
     if (blockHour < hourNow) {
@@ -18,6 +21,7 @@ $(".time-block").each(function () {
     $(this).children(".description").val(localStorage.getItem((this).id));
 })
 
+// saves the content of the time block to local storage
 $(".saveBtn").click(function () {
     var hour = $(this).parent().attr("id");
     var task = $(this).siblings(".description").val();
